c271b3095e747321d4a03cd37a62d15df2564f53
ayulockin <mein2work@gmail.com>
2019-04-11 16:24:14 +0530

    encoding+board added

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -17,3 +17,65 @@ class Move():
         @classmethod
         def resign(cls):
             return Move(is_resign=True)
+
+
+
+class GoString():
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+    def remove_liberty(self, point):
+        return self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        return self.liberties.add(point)
+
+    def merge_with(self, gostring):
+        assert gostring.color == self.color
+        combined_stones = self.stones | gostring.stones
+        combined_liberties = (self.liberties | gostring.liberties) - combined_liberties
+
+        return GoString(self.color, combined_stones,
+                        combined_liberties)
+
+    def num_liberties(self):
+        return len(self.liberties)
+
+
+class Board():
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        ## To keep track of board state internally.
+        ## Store strings of stones in dict.
+        self._grid = dict()
+
+    def place_stone(self, player, point):
+        '''
+        inspect all the neighbouring stones for the given point for liberties.
+
+        For the given point check for all the neighbouring stones and
+        calculate liberties.
+        '''
+        adj_same_color = list()
+        adj_diff_color = list()
+        liberties = []
+
+
+
+
+    ## Utilities
+
+    def is_on_grid(self, point):
+        
+        
+
+
+
+
+
+
+
+
